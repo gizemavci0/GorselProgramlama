cd74c8e89356b03c0457bb9786514b7351bc1465
Gizem Avcı <152634662+gizemavci0@users.noreply.github.com>
2024-01-02 17:14:48 +0300

    Add files via upload

---
+++ asal_mi/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace asal_mi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(438, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ASAL MI?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ asal_mi/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace asal_mi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBox1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            label1.Text = "ASAL MI ?";
+            if (e.KeyCode == Keys.Enter)
+            {
+                int sayi=0;
+                try
+                {
+                    sayi = Convert.ToInt32(textBox1.Text);
+                    bool asal_mi = true;
+                    for (int i = sayi - 1; i >= 2; i--)
+                    {
+
+                        if (sayi % i == 0)
+                        {
+                            asal_mi = false;
+                            break;
+                        }
+                    }
+                    if (asal_mi.Equals(true))
+                    {
+                        label1.Text = "ASAL";
+
+                    }
+                    else
+                    {
+                        label1.Text = "ASAL DEĞİL";
+                    }
+
+                    textBox1.Text = " ";
+                }
+
+                catch
+                {
+                    MessageBox.Show("Hatali parametre girildi");
+                    textBox1.Text = " ";
+
+                }
+                      
+            }
+
+        }
+    }
+}
